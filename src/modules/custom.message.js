35e97c116d230386f12c9d9a1d40dd3e300cb977
Aleksey Beketov <alex311271@gmail.com>
2023-12-03 18:19:05 +0300

    add custom message

---
+++ src/modules/custom.message.js
@@ -0,0 +1,32 @@
+import { RandomQuote } from '../core/random.quote'
+import { Module } from '../core/module'
+
+export class RandomText extends Module {
+	constructor(type, text) {
+		super(type, text)
+		this.randomText = new RandomQuote()
+	}
+
+	trigger() {
+		const text = document.querySelector('.module__custom-message')
+
+		const item = document.querySelector(`li[data-type = '${this.type}']`)
+		item.addEventListener('click', event => {
+			const { target } = event
+			if (target) {
+				this.randomText.getRandomQuote()
+				if (text) {
+					text.remove
+				}
+			}
+			console.log(text)
+		})
+		throw new Error(
+			`Trigger method should be implemented in module "${this.type}"`
+		)
+	}
+
+	toHTML() {
+		return `<li class="menu-item" data-type="${this.type}">${this.text}</li>`
+	}
+}
