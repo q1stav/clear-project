ff48601a4df014e36bcebd3c63b611e0c4301e3f
q1stav <kmi762@yandex.ru>
2023-12-03 16:31:24 +0300

    init

--- src/app.js
+++ src/app.js
@@ -1,2 +1,6 @@
 import './styles.css'
 
+const contextMenu = new ContextMenu()
+contextMenu.open()
+contextMenu.close()
+contextMenu.add()
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,37 @@
 import {Menu} from './core/menu'
-
+const menu = document.querySelector('.menu')
 export class ContextMenu extends Menu {
+    open() {
+        document.addEventListener('contextmenu', e => {
+            e.preventDefault() 
 
+            const clientWidth = document.body.clientWidth
+            const clientHeight = document.body.clientHeight
+
+            menu.style.display = 'block'
+
+            const menuWidth = menu.clientWidth
+            const menuHeight = menu.clientHeight
+
+            if (clientWidth - e.clientX > menuWidth) {
+                menu.style.left = `${e.clientX}px`
+            } else {
+                menu.style.left = `${clientWidth - menuWidth}px`
+            }
+
+            if (clientHeight - e.clientY > menuHeight) {
+                menu.style.top = `${e.clientY}px`
+            } else {
+                menu.style.top = `${clientHeight - menuHeight}px`
+            }
+        })
+    }
+
+    close() {
+        document.addEventListener('click', () => menu.style.display = 'none')
+    }
+
+    add() {
+      
+    }
 }
